8651ffa032a4068c60ba72a7cf59a9f3b4be2170
tojoirinah <tojoirinah@gmail.com>
2021-01-06 13:56:26 +0100

    Ajoutez des fichiers projet.

---
+++ Bibliotheque.Api/App_Start/SwaggerConfiguration.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Bibliotheque.Api.App_Start
+{
+    public static class SwaggerConfiguration
+    {
+        public static void AddSwaggerGenExtension(this IServiceCollection services)
+        {
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+                // resolve Failed to load API definition.
+                c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+
+                // Swagger 2.+ support
+                var security = new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                };
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(security);
+            });
+        }
+    }
+}
