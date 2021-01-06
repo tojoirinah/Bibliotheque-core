1a0b968d709a52064e4a27737bbf71b3f2082b69
tojoirinah <tojoirinah@gmail.com>
2021-01-06 19:16:40 +0100

    Add modification

---
+++ Bibliotheque.Api/Controllers/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Bibliotheque.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(ILogger<UsersController> logger)
+        {
+            _logger = logger;
+        }
+
+        readonly IEnumerable<User> _list = new List<User>()
+            {
+                new User(){ Id = Guid.NewGuid(), Name = "Statam", FirstName = "Jason"},
+                new User(){ Id = Guid.NewGuid(), Name = "Lee", FirstName = "Bruce"},
+                new User(){ Id = Guid.NewGuid(), Name = "Stalone", FirstName = "Sylvester"},
+                new User(){ Id = Guid.NewGuid(), Name = "Diesel", FirstName = "Vin"},
+            };
+
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return _list;
+        }
+
+        [HttpGet("{name}")]
+        public User GetByName(string name)
+        {
+            var user = _list.FirstOrDefault(x => x.Name == name);
+            return user;
+        }
+    }
+}
---
+++ Bibliotheque.Api/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Bibliotheque.Api
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+    }
+}
